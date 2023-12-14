0b85b1d04c6fb9ae6dbc8464177c51a9a331a7af
momotan <momotaha039@gmail.com>
2023-12-14 04:14:11 +0200

    switch navbar to dark

--- inc/navbar.php
+++ inc/navbar.php
@@ -4,7 +4,7 @@ function isActivePage($page){
   return $_SERVER['PHP_SELF']==$pages.'/'.$page?'active':'';
 }
 ?>
-<nav class="navbar navbar-expand-lg bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
     <a class="navbar-brand" href="/ecommerce">Ecommerce App</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
