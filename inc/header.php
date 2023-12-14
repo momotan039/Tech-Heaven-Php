db70179bf30809d3b4c103bfc86d536834343352
momotan <momotaha039@gmail.com>
2023-12-14 20:22:55 +0200

    add font awesome cdn

--- inc/header.php
+++ inc/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="<?php echo $style?>/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"/>
     <link rel="stylesheet" href="<?php echo $style?>/style.css">
 </head>
 <body>
