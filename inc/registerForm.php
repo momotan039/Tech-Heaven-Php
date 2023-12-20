d4b861b0ebe1cec6ffec5635bff9bb3a7425a6f3
momotan <momotaha039@gmail.com>
2023-12-20 18:10:58 +0200

    complete crud operation by adding edit mode

--- inc/registerForm.php
+++ inc/registerForm.php
@@ -1,31 +1,43 @@
-<div class="card">
+<div class="card mt-4">
     <div class="card-body">
-        <h1 class="card-text text-center text-primary"><?php echo !$isEditForm?'Registeration':'Edit'?> Form</h1>
+        <h1 class="card-text text-center text-primary">
+            <?php echo !$isEditForm ? 'Registeration' : 'Edit' ?> Form
+        </h1>
     </div>
-    <form method="post" id="<?php echo $isEditForm?'editForm':''?>">
+    <form method="post" id="<?php echo $isEditForm ? 'editForm' : '' ?>">
         <div class="px-3">
+            
             <div class="form-group">
+            <?php
+            if($isEditForm):
+                echo '<input type="text" name="id" hidden>';
+            endif;
+            ?>
                 <i class="fa-solid fa-user"></i>
                 <label>User Name:</label>
-                <input required name="userName" class="form-control" type="text" value="<?php isset($user)?$user['userName']:''?>">
+                <input required name="userName" class="form-control" type="text" value="<?php isset($user) ? $user['userName'] : '' ?>">
             </div>
             <div class="form-group">
                 <i class="fa-solid fa-envelope"></i>
                 <label>Email:</label>
-                <input required name="email" class="form-control" type="email" value="<?php isset($user)?$user['email']:''?>">
+                <input required name="email" class="form-control" type="email" value="<?php isset($user) ? $user['email'] : ''?>">
             </div>
             <?php
-            if(!$isEditForm)
-            echo '
+            if (!$isEditForm):
+                echo '
                 <div class="form-group">
                 <i class="fa-solid fa-lock"></i>
                 <label>Password:</label>
                 <input required name="password" class="form-control" type="password"">
                  </div>
                 ';
+            endif;
             ?>
             <div class="text-center my-2">
-                <input type="submit" class="btn btn-success" value="<?php echo !$isEditForm?'Register':'Edit'?>">
+            <?php echo !$isEditForm ? '<input type="submit" class="btn btn-success" value="Register">' 
+                  : '<input type="submit" class="btn btn-success" value="Edit">
+                     <input type="text" name="edit" hidden> 
+                     '  ?>
             </div>
         </div>
     </form>
--- pages/home.php
+++ pages/home.php
@@ -5,17 +5,21 @@ require '../config/db2.php'
 ?>
 
 <?php
-// get user by id
-if ($_SERVER['REQUEST_METHOD'] === 'GET' && isset($_GET['id'])) {
-    $stms=$con->prepare('SELECT * FROM users WHERE Id=:Id');
-    $stms->execute([':Id'=>$_GET['id']]);
-    $user=$stms->fetch(PDO::FETCH_ASSOC);
-    echo json_encode($user);
-    exit;
+// handel editing user
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['edit'])) {
+    //UPDATE users SET name = :newName WHERE id = :userID
+    $stmt = $con->prepare('UPDATE users SET UserName=:username , Email=:email WHERE Id=:id');
+    $stmt->execute([':id'=>$_POST['id'],':username' => $_POST['userName'], ':email' => $_POST['email']]);
+    echo '
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>Yeah,</strong>User Updated successfully!!!
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    ';
 }
 
 // handel adding user
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['id'])) {
+elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['id'])) {
     $stmt = $con->prepare('INSERT INTO users(UserName,Email,Password) VALUES(:username,:email,:password)');
     $stmt->execute([':username' => $_POST['userName'], ':email' => $_POST['email'], ':password' => sha1($_POST['password'])]);
     echo '
@@ -27,7 +31,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['id'])) {
 }
 
 // handle omiting specific user
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
+elseif ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id'])) {
     $stms=$con->prepare('DELETE FROM users WHERE Id=:Id');
     $stms->execute([':Id'=>$_POST['id']]);
     echo '
@@ -54,50 +58,51 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <div class="card">
         <div class="card-body">
         <div class="card-title text-center text-muted"><h1>LIST OF USERS</h1></div>
-            <table class="table table-dark table-striped table-hover text-center">
-                <thead>
-                    <tr>
-                        <td>UserName</td>
-                        <td>Email</td>
-                        <td>Operation</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php
-                    foreach ($users as $user) {
-                        $isEditForm=true;
-                        $includedContent = file_get_contents('../inc/registerForm.php');
-                        echo <<<HTML
-                        <tr>
-                            <td>{$user['UserName']}</td>
-                            <td>{$user['Email']}</td>
-                            <td class="d-flex justify-content-center">
-                                <form method="post">
-                                <input hidden name="id" type="text" value={$user['Id']}>
-                                <button name="" class="btn"><i class="fa-solid fa-trash text-danger"></i></button>
-                                </form>
-                                <div>
-                                <button onclick="editUser({$user['Id']})" class="btn"><i class="fa-solid fa-edit text-primary" data-bs-toggle="modal" data-bs-target="#exampleModal{$user['Id']}"></i></button>
-                                </div>
-                            </td>
-                            <div class="modal fade" id="exampleModal{$user['Id']}" tabindex="-1">
-                                <div class="modal-dialog">
-                                    <div class="modal-content">
-                                    <div class="modal-header">
-                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                    </div>
-                                    <div class="modal-body">
-                                        {$includedContent}
-                                    </div>
-                                    </div>
-                                </div>
-                                </div>
-                        </tr>
-                HTML;
-                    }
-                    ?>
-                </tbody>
-            </table>
+        <div class='modal fade' id='exampleModal' tabindex='-1'>
+        <div class='modal-dialog'>
+            <div class='modal-content'>
+                <div class='modal-header'>
+                    <button type='button' class='btn-close' data-bs-dismiss='modal' aria-label='Close'></button>
+                </div>
+                <div class='modal-body'>
+                <?php $isEditForm=true; include '../inc/registerForm.php'?>
+                </div>
+            </div>
+        </div>
+    </div>
+        <table class="table table-dark table-striped table-hover text-center">
+    <thead>
+        <tr>
+            <td>UserName</td>
+            <td>Email</td>
+            <td>Operation</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($users as $user) {
+            $isEditForm = true;
+            $data = file_get_contents("../inc/registerForm.php");
+            echo "
+            <tr>
+                <td>{$user['UserName']}</td>
+                <td>{$user['Email']}</td>
+                <td class='d-flex justify-content-center'>
+                    <form method='post'>
+                        <input hidden name='id' type='text' value={$user['Id']}>
+                        <button name='' class='btn'><i class='fa-solid fa-trash text-danger'></i></button>
+                    </form>
+                    <div>
+                        <button onclick='passUser({$user['Id']})' class='btn' data-bs-toggle='modal' data-bs-target='#exampleModal'><i class='fa-solid fa-edit text-primary'></i></button>
+                    </div>
+                </td>
+            </tr>
+        ";
+        }
+        ?>
+    </tbody>
+</table>
+
         </div>
     </div>
 </div>
@@ -105,15 +110,14 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 </div>
 
 <script>
-    function editUser(id){
-        fetch('./?id='+id,{
-            headers: {
-        'Accept': 'application/json',
-    },
-        })
+    function passUser(id){
+        fetch('../api/users.php/?id='+id)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
+            const inputs=document.querySelectorAll('#editForm input')
+            inputs[0].value=data.Id;
+            inputs[1].value=data.UserName;
+            inputs[2].value=data.Email;
         })
         .catch(error => {
             console.error('Error:', error);
