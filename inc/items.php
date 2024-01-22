8bfd5da8bec298a1e81e4c3884f6306d905309fe
momotan <momotaha039@gmail.com>
2024-01-22 03:25:04 +0200

    create items component

--- inc/items.php
+++ inc/items.php
@@ -1,19 +1,44 @@
 <?php
-$items = array(1,2,3,4,5);
+$items = array(
+    array(
+        'id' => 1,
+        'name' => 'Smartphone',
+        'cat' => 'Mobile',
+        'description' => 'A powerful handheld device with various features.',
+        'price' => 599.99
+    ),
+    array(
+        'id' => 2,
+        'name' => 'Laptop',
+        'cat' => 'Computer',
+        'description' => 'Portable computer with a keyboard and display.',
+        'price' => 999.99
+    ),
+    array(
+        'id' => 3,
+        'name' => 'Smart TV',
+        'cat' => 'Television',
+        'description' => 'Television with internet connectivity and smart features.',
+        'price' => 799.99
+    ),
+);
 ?>
 
 <div class="row justify-content-center gap-3">
     <?php
     foreach ($items as $item) {
     ?>
-        <div class="col-sm-6 col-l-3 card shadow-sm">
+        <div class="col-sm-5 col-md-3 card shadow-sm">
+            <h3 class="bg-success text-light rounded text-center"><?php echo $item['name']?></h3>
             <img src="../template/img/laptop.jpg" class="card-img-top" alt="Image">
             <div class="card-body">
-                <h5 class="card-title">Laptop</h5>
-                <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, laboriosam.</p>
-                <div class="flex justify-content-space-between">
-                <a href="#" class="btn btn-primary">show detials</a>
-                <span class="btn-secondary">Price:25$</span>
+                <h5 class="card-title text-warning bg-dark rounded text-center p-2"><i class="fa fa-tag"></i><?php echo $item['cat']?></h5>
+                <p class="card-text"><?php echo $item['description']?></p>
+                <div class="row">
+                    <a href="<?php echo $pages?>/itemDetails.php?id=<?php echo $item['id']?>" class="btn btn-primary col-md-6 fw-bold">show detials</a>
+                    <b class="col-md-6 text-center align-middle mt-1">
+                        <h6  class="text-warning bg-dark rounded text-center p-2 align-middle"><?php echo $item['price']?>$</h6>
+                    </b>
                 </div>
             </div>
         </div>
