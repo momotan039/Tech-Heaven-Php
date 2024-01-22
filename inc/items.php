30582602ddac23f494645843e7932daadc09bccd
momotan <momotaha039@gmail.com>
2024-01-22 18:57:42 +0200

    calling items from database

--- inc/items.php
+++ inc/items.php
@@ -1,27 +1,6 @@
 <?php
-$items = array(
-    array(
-        'id' => 1,
-        'name' => 'Smartphone',
-        'cat' => 'Mobile',
-        'description' => 'A powerful handheld device with various features.',
-        'price' => 599.99
-    ),
-    array(
-        'id' => 2,
-        'name' => 'Laptop',
-        'cat' => 'Computer',
-        'description' => 'Portable computer with a keyboard and display.',
-        'price' => 999.99
-    ),
-    array(
-        'id' => 3,
-        'name' => 'Smart TV',
-        'cat' => 'Television',
-        'description' => 'Television with internet connectivity and smart features.',
-        'price' => 799.99
-    ),
-);
+include '../app/items/get.php';
+$items = getItems();
 ?>
 
 <div class="row justify-content-center gap-3">
@@ -29,15 +8,15 @@ $items = array(
     foreach ($items as $item) {
     ?>
         <div class="col-sm-5 col-md-3 card shadow-sm">
-            <h3 class="bg-success text-light rounded text-center"><?php echo $item['name']?></h3>
+            <h3 class="bg-success text-light rounded text-center"><?php echo $item['Name']?></h3>
             <img src="../template/img/laptop.jpg" class="card-img-top" alt="Image">
             <div class="card-body">
-                <h5 class="card-title text-warning bg-dark rounded text-center p-2"><i class="fa fa-tag"></i><?php echo $item['cat']?></h5>
-                <p class="card-text"><?php echo $item['description']?></p>
+                <h5 class="card-title text-warning bg-dark rounded text-center p-2"><i class="fa fa-tag"></i><?php echo $item['Cat_Name']?></h5>
+                <p class="card-text"><?php echo $item['Description']?></p>
                 <div class="row">
-                    <a href="<?php echo $pages?>/itemDetails.php?id=<?php echo $item['id']?>" class="btn btn-primary col-md-6 fw-bold">show detials</a>
+                    <a href="<?php echo $pages?>/itemDetails.php?id=<?php echo $item['Id']?>" class="btn btn-primary col-md-6 fw-bold">show detials</a>
                     <b class="col-md-6 text-center align-middle mt-1">
-                        <h6  class="text-warning bg-dark rounded text-center p-2 align-middle"><?php echo $item['price']?>$</h6>
+                        <h6  class="text-warning bg-dark rounded text-center p-2 align-middle"><?php echo $item['Price']?>$</h6>
                     </b>
                 </div>
             </div>
--- pages/itemDetails.php
+++ pages/itemDetails.php
@@ -1,28 +1,21 @@
 <?php
 require '../config/init.php';
-require '../config/db2.php';
 require '../inc/header.php';
-
+require '../app/items/get.php';
 $id = $_GET['id'];
 if (!isset($id) || empty($id)) {
     header('location:404.php');
 }
-$item = array(
-    'id' => 1,
-    'name' => 'Smartphone',
-    'cat' => 'Mobile',
-    'description' => 'A powerful handheld device with various features.',
-    'price' => 599.99
-);
+$item=getItem($id);
 ?>
-<div class="container">
-    <h1 class="bg-warning text-center"><?php echo $item['name'] ?></h1>
+<div class="container mt-3">
+    <h1 class="bg-warning text-center mb-5"><?php echo $item['Name'] ?></h1>
     <div class="row shadow">
         <img src="../template/img/laptop.jpg" class="col-md-6" alt="image">
         <div class="d-flex flex-column justify-content-between detials col-md-6">
-            <h3><?php echo $item['description'] ?></h3>
+            <h3><?php echo $item['Description'] ?></h3>
             <div class="row gap-1 mb-1">
-                <h6 class="text-warning bg-dark rounded text-center col-5 py-3"><?php echo $item['price'] ?>$</h6>
+                <h6 class="text-warning bg-dark rounded text-center col-5 py-3"><?php echo $item['Price'] ?>$</h6>
                 <button class="btn col-5 btn-warning fw-bold">add to cart <i class="fa fa-cart-shopping"></i></button>
             </div>
         </div>
