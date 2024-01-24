c897ecfed5fa815d74e038a8ee2371d5865401a0
momotan <momotaha039@gmail.com>
2024-01-24 19:28:09 +0200

    refactor get items functions

--- app/items/get.php
+++ app/items/get.php
@@ -1,13 +1,13 @@
 <?php
 function getItem($id){
     global $con;
-    $stm=$con->prepare('SELECT * FROM products WHERE Id=?');
-    $stm->execute(array($id));
+    $stm=$con->prepare('SELECT * FROM products INNER JOIN categories on CatId=categories.Cat_Id WHERE Id=?');
+    $stm->execute([$id]);
     return $stm->fetch(PDO::FETCH_ASSOC);
 }
 function getItems(){
     global $con;
-    $stm=$con->prepare('SELECT * FROM products INNER JOIN categories on CatId=categories.Id');
+    $stm=$con->prepare('SELECT * FROM products INNER JOIN categories on CatId=categories.Cat_Id');
     $stm->execute();
     return $stm->fetchAll(PDO::FETCH_ASSOC);
 }
