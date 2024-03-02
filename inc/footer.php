4d9cb252df1ce63e8cf3ce26858def13107748b2
momotan <momotaha039@gmail.com>
2024-03-03 00:08:33 +0200

    complete building footer

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,31 @@
-<script src="<?php echo $js?>/bootstrap.bundle.min.js"></script>
+<!-- <script src="<?php echo $js ?>/bootstrap.bundle.min.js"></script> -->
+<footer>
+    <div class="row mb-1">
+        <a class="col-4" href="">
+            <h5 >About us</h5>
+        </a>
+        <a class="col-4" href="">
+            <h5 >Customer Service</h5>
+        </a>
+        <a class="col-4" href="">
+            <h5 >Privacy Policy</h5>
+        </a>
+    </div>
+    <div class="mb-1 d-flex gap-2 justify-content-center">
+        <a   href="">
+        <i class="fa-brands fa-facebook"></i>
+        </a>
+        <a href="">
+        <i class="fa-brands fa-twitter"></i>
+        </a>
+        <a href="">
+        <i class="fa-brands fa-instagram"></i>
+        </a>
+    </div>
+    <div class="row">
+       <div class="col"> © 2024, Tech Heaven</div>
+    </div>
+</footer>
 </body>
+
 </html>
