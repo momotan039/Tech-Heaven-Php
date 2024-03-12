0845bf969955140ae13c54e8598ef6e23deaaa43
momotan <momotaha039@gmail.com>
2024-03-12 21:47:17 +0200

    create reusable listed item

---
+++ inc/listed-item.php
@@ -0,0 +1,7 @@
+<a href="product-details.php?id=<?php echo $item['product_id']?>">
+    <div class="fragment-item">
+        <img class="card" src="<?php echo $item['path']?>">
+        <h3 class="title"><?php echo $item['name'] ?></h3>
+        <span class="price">$<?php echo $item['price'] ?></span>
+    </div>
+</a>
