ae1a10cf7353087591ac303c8b96017642417868
momotan <momotaha039@gmail.com>
2024-03-12 21:48:56 +0200

    create pagination component

---
+++ inc/pagination.php
@@ -0,0 +1,14 @@
+  <ul class="pagination justify-content-center">
+      <li class="page-item">
+          <a class="page-link" href="#">
+              <span aria-hidden="true">&laquo;</span>
+          </a>
+      </li>
+      <li class="page-item"><a class="page-link" href="./?page=1">1</a></li>
+      <li class="page-item"><a class="page-link" href="./?page=2">2</a></li>
+      <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+              <span aria-hidden="true">&raquo;</span>
+          </a>
+      </li>
+  </ul>
