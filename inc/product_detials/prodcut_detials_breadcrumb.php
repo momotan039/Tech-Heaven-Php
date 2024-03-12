b2d287fb2802b04f2bf805a5c7c9f8596ed43730
momotan <momotaha039@gmail.com>
2024-03-12 22:21:39 +0200

    create reusable bredcrumb for product detials page

---
+++ inc/product_detials/prodcut_detials_breadcrumb.php
@@ -0,0 +1,7 @@
+<div class="breadcrumb mb-4">
+        <ul>
+            <li><?php echo $product['category_name'] ?></li>
+            <li><?php echo $product['subcategory_name'] ?></li>
+            <li><?php echo $product['name'] ?></li>
+        </ul>
+    </div>
