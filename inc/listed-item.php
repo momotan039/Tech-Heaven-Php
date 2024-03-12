6d85f0a4c2d2c180cc243fffd96985d7e21e3b06
momotan <momotaha039@gmail.com>
2024-03-12 22:17:23 +0200

    show static image in case there is no image

--- inc/listed-item.php
+++ inc/listed-item.php
@@ -1,6 +1,6 @@
 <a href="product-details.php?id=<?php echo $item['product_id']?>">
     <div class="fragment-item">
-        <img class="card" src="<?php echo $item['path']?>">
+        <img class="card" src="<?php echo !empty($item['path'])?$item['path']:'https://via.placeholder.com/300'?>">
         <h3 class="title"><?php echo $item['name'] ?></h3>
         <span class="price">$<?php echo $item['price'] ?></span>
     </div>
